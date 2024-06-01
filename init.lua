623a17e35b5419128b63abeb9e975eac6997532a
arashjp8 <arashjafarpour723@gmail.com>
2024-06-01 09:02:39 +0330

    [refactor] - init.lua updated seperate each plugin

--- init.lua
+++ init.lua
@@ -17,53 +17,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-  "rockyzhang24/arctic.nvim",
-  dependencies = { "rktjmp/lush.nvim" },
-  name = "arctic",
-  branch = "main",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme arctic")
-  end
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {'nvim-treesitter/nvim-treesitter', build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-    }
-  },
-}
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<Space><Space>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-vim.keymap.set('n', '<leader>t', ":Neotree filesystem reveal left toggle<CR>", {})
 
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"c", "cpp", "lua", "javascript", "typescript", "vim", "bash", "css", "html"},
-  highlight = {enable = true},
-  indent = {enable = true}
-})
-
-local neo_tree = require("neo-tree")
-neo_tree.setup({
-  window = {
-    position = "left",
-    width = 30,
-  }
-})
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,20 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+    },
+    config = function() 
+      local neo_tree = require("neo-tree")
+      neo_tree.setup({
+        window = {
+        position = "left",
+        width = 30,
+        }
+      })
+      
+      vim.keymap.set('n', '<leader>t', ":Neotree filesystem reveal left toggle<CR>", {})
+    end,
+}
