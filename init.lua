0ebb4bbfd6202b955ea4eb667211b95770fbcf29
arashjp8 <arashjafarpour723@gmail.com>
2024-06-01 07:19:18 +0330

    [checkpoint] - telescope fuzzy finder and new color scheme added

--- init.lua
+++ init.lua
@@ -16,8 +16,25 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-local plugins = {}
+local plugins = {
+  {
+  "rockyzhang24/arctic.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+  name = "arctic",
+  branch = "main",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme arctic")
+  end
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+}
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<Space><Space>', builtin.find_files, {})
