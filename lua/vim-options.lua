9ac4e5e5c1361d2814db8c84356573a47bb0255f
arashjp8 <arashjafarpour723@gmail.com>
2024-06-02 08:57:32 +0330

    [checkpoint] - tmux navigator added

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,4 +8,11 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv")
 
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
