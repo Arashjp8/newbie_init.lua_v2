c9f9a73f779961870bfb28416066d32f6b0679f1
arashjp8 <arashjafarpour723@gmail.com>
2024-06-03 14:42:28 +0330

    [feature] - todo-comments added

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {},
+	config = function()
+		require("todo-comments").setup()
+	end,
+}
+
+-- PERF:
+-- HACK:
+-- TODO:
+-- NOTE:
+-- FIX:
+-- WARNING:
