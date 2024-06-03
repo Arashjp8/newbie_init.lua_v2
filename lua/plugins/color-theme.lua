1c9ba6a2e3ba474d2a0ca61e14f0fd23b03e135e
arashjp8 <arashjafarpour723@gmail.com>
2024-06-03 06:04:03 +0330

    [feature] - format on save added

--- lua/plugins/color-theme.lua
+++ lua/plugins/color-theme.lua
@@ -2,7 +2,7 @@ return {
   "rockyzhang24/arctic.nvim",
   dependencies = { "rktjmp/lush.nvim" },
   name = "arctic",
-  branch = "main",
+  branch = "v2",
   priority = 1000,
   config = function()
     vim.cmd("colorscheme arctic")
