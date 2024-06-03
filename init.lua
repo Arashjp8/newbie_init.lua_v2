98a7e92b32d303f327a81954420a0fc2243149f4
arashjp8 <arashjafarpour723@gmail.com>
2024-06-03 06:22:53 +0330

    [feature] - trouble.nvim added + better overall performance

--- init.lua
+++ init.lua
@@ -1,17 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+vim.loader.enable()
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
-
-local opts = {}
 
 require("vim-options")
 require("replace-var")
