dbaeec014d9efa9610c5666fc8e86000c9974d63
arashjp8 <arashjafarpour723@gmail.com>
2024-06-04 09:47:59 +0330

    [feature] - added noice for better cmd ui

--- lua/plugins/color-theme.lua
+++ lua/plugins/color-theme.lua
@@ -30,3 +30,18 @@ return {
 -- 		vim.cmd.colorscheme("tokyonight-night")
 -- 	end,
 -- }
+
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   config = function()
+--     require("rose-pine").setup({
+--       disable_background = true,
+--       styles = {
+--         italic = false,
+--       },
+--     })
+--
+--     vim.cmd("colorscheme rose-pine-moon")
+--   end,
+-- }
