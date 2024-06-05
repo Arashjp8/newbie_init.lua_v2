9ab35fa0270f9c1c5079caf509a2607debba3a86
arashjp8 <arashjafarpour723@gmail.com>
2024-06-05 11:20:02 +0330

    [feature] - jump functionality with flash.nvim

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,19 +13,19 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 
-		-- h
+		-- it was originally ctrl + h
 		vim.keymap.set("n", "<leader>1", function()
 			harpoon:list():select(1)
 		end)
-		-- t
+		-- it was originally ctrl + t
 		vim.keymap.set("n", "<leader>2", function()
 			harpoon:list():select(2)
 		end)
-		-- n
+		-- it was originally ctrl + n
 		vim.keymap.set("n", "<leader>3", function()
 			harpoon:list():select(3)
 		end)
-		-- s
+		-- it was originally ctrl + s
 		vim.keymap.set("n", "<leader>4", function()
 			harpoon:list():select(4)
 		end)
