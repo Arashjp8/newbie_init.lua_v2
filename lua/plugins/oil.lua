6d653045a26a16de45adb8027b31785d36bf3048
arashjp8 <arashjafarpour723@gmail.com>
2024-12-06 18:20:30 +0330

    [feature] - undotree added some other unused plugins got deleted

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,19 +1,27 @@
 return {
-	"stevearc/oil.nvim",
-	config = function()
-		local oil = require("oil")
-		oil.setup({
-			default_file_explorer = true,
-			view_options = {
-				show_hidden = true,
-				is_always_hidden = function(name, _)
-					return name == ".." or name == ".git"
-				end,
-			},
-			win_options = {
-				wrap = true,
-			},
-		})
-		vim.keymap.set("n", "-", oil.toggle_float, {})
-	end,
+  {
+    "stevearc/oil.nvim",
+    config = function()
+      local oil = require("oil")
+      oil.setup({
+        default_file_explorer = true,
+        view_options = {
+          show_hidden = true,
+          is_always_hidden = function(name, _)
+            return name == ".." or name == ".git"
+          end,
+        },
+        win_options = {
+          wrap = true,
+        },
+      })
+      vim.keymap.set("n", "-", oil.toggle_float, {})
+    end,
+  },
+  {
+    "nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup({})
+    end,
+  },
 }
