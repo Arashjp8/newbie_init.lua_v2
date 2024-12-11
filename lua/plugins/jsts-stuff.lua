5e0defd43bd1f11c35c29b0a5e40c1bd43313b2d
arashjp8 <arashjafarpour723@gmail.com>
2024-12-11 10:12:38 +0330

    [feature] - tailwindcss colorizer cmp added

--- lua/plugins/jsts-stuff.lua
+++ lua/plugins/jsts-stuff.lua
@@ -16,4 +16,13 @@ return {
       require("nvim-ts-autotag").setup({})
     end,
   },
+  {
+    "roobert/tailwindcss-colorizer-cmp.nvim",
+    -- optionally, override the default options:
+    config = function()
+      require("tailwindcss-colorizer-cmp").setup({
+        color_square_width = 2,
+      })
+    end,
+  },
 }
