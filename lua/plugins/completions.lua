b29207840bc3be7f89d3ecce251c8e6ba1f1d02a
arashjp8 <arashjafarpour723@gmail.com>
2025-01-07 22:56:45 +0330

    [checkpoint] - lualine changes

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -2,87 +2,87 @@
 -- Plugin Configuration: nvim-cmp LSP source
 ---------------------------------------------------------------------
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
 
-	---------------------------------------------------------------------
-	-- Plugin for LuaSnip with dependencies for better snippet support
-	---------------------------------------------------------------------
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip", -- Dependency for LuaSnip integration with nvim-cmp
-			"rafamadriz/friendly-snippets", -- Additional snippets for LuaSnip
-		},
-	},
+  ---------------------------------------------------------------------
+  -- Plugin for LuaSnip with dependencies for better snippet support
+  ---------------------------------------------------------------------
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",  -- Dependency for LuaSnip integration with nvim-cmp
+      "rafamadriz/friendly-snippets", -- Additional snippets for LuaSnip
+    },
+  },
 
-	---------------------------------------------------------------------
-	-- Plugin for nvim-cmp with custom configuration
-	---------------------------------------------------------------------
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			-- Load VSCode style snippets
-			require("luasnip.loaders.from_vscode").lazy_load()
-			cmp.setup({
-				---------------------------------------------------------------------
-				-- Snippet settings for nvim-cmp
-				---------------------------------------------------------------------
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				---------------------------------------------------------------------
-				-- Window settings for completion and documentation
-				---------------------------------------------------------------------
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				---------------------------------------------------------------------
-				-- Key mapping for completion actions
-				---------------------------------------------------------------------
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4), -- Scroll docs up
-					["<C-f>"] = cmp.mapping.scroll_docs(4), -- Scroll docs down
-					["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion
-					["<C-e>"] = cmp.mapping.abort(), -- Abort completion
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm completion
-				}),
+  ---------------------------------------------------------------------
+  -- Plugin for nvim-cmp with custom configuration
+  ---------------------------------------------------------------------
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      -- Load VSCode style snippets
+      require("luasnip.loaders.from_vscode").lazy_load()
+      cmp.setup({
+        ---------------------------------------------------------------------
+        -- Snippet settings for nvim-cmp
+        ---------------------------------------------------------------------
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        ---------------------------------------------------------------------
+        -- Window settings for completion and documentation
+        ---------------------------------------------------------------------
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        ---------------------------------------------------------------------
+        -- Key mapping for completion actions
+        ---------------------------------------------------------------------
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),      -- Scroll docs up
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),       -- Scroll docs down
+          ["<C-Space>"] = cmp.mapping.complete(),       -- Trigger completion
+          ["<C-e>"] = cmp.mapping.abort(),              -- Abort completion
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Confirm completion
+        }),
 
-				---------------------------------------------------------------------
-				-- Sources for completion suggestions
-				---------------------------------------------------------------------
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" }, -- LSP source
-					{ name = "luasnip" }, -- LuaSnip source
-				}, {
-					{ name = "buffer" }, -- Buffer source
-				}),
+        ---------------------------------------------------------------------
+        -- Sources for completion suggestions
+        ---------------------------------------------------------------------
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" }, -- LSP source
+          { name = "luasnip" }, -- LuaSnip source
+        }, {
+          { name = "buffer" }, -- Buffer source
+        }),
 
-				---------------------------------------------------------------------
-				-- Formatting settings for completion items
-				---------------------------------------------------------------------
-				-- NOTE: installed in lua/plugins/jsts-stuff.lua file
-				formatting = {
-					format = require("tailwindcss-colorizer-cmp").formatter, -- Tailwind CSS colorizer
-					fields = { "abbr", "kind", "menu" },
-					expandable_indicator = true, -- Show expandable indicator for snippets
-				},
-			})
+        ---------------------------------------------------------------------
+        -- Formatting settings for completion items
+        ---------------------------------------------------------------------
+        -- NOTE: installed in lua/plugins/jsts-stuff.lua file
+        formatting = {
+          format = require("tailwindcss-colorizer-cmp").formatter, -- Tailwind CSS colorizer
+          fields = { "abbr", "kind", "menu" },
+          expandable_indicator = true,                        -- Show expandable indicator for snippets
+        },
+      })
 
-			---------------------------------------------------------------------
-			-- SQL completion configuration
-			---------------------------------------------------------------------
-			cmp.setup.filetype({ "sql" }, {
-				sources = {
-					{ name = "vim-dadbod-completion" },
-					{ name = "buffer" },
-				},
-			})
-		end,
-	},
+      ---------------------------------------------------------------------
+      -- SQL completion configuration
+      ---------------------------------------------------------------------
+      cmp.setup.filetype({ "sql" }, {
+        sources = {
+          { name = "vim-dadbod-completion" },
+          { name = "buffer" },
+        },
+      })
+    end,
+  },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,28 +1,153 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = "|",
-        section_separators = { left = "", right = "" },
-        -- section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diagnostics" },
-        lualine_c = { "filename" },
-        lualine_x = {
-          -- "encoding",
-          -- "fileformat",
-          "filetype",
-        },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-    },
-  },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		event = "VeryLazy",
+		opts = function()
+			local colors = {
+				bg = "#16161E", -- background
+				fg = "#a9b1d6", -- foreground
+				yellow = "#e0af68",
+				cyan = "#7dcfff",
+				darkblue = "#414868", -- black
+				green = "#73daca",
+				orange = "#e0af68", -- reused yellow as orange is not explicitly in Tokyo Night
+				violet = "#bb9af7", -- magenta
+				magenta = "#bb9af7",
+				blue = "#7aa2f7",
+				red = "#f7768e",
+				white = "#c0caf5", -- explicitly added white for potential use
+			}
+
+			local conditions = {
+				buffer_not_empty = function()
+					return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+				end,
+				hide_in_width = function()
+					return vim.fn.winwidth(0) > 80
+				end,
+				check_git_workspace = function()
+					local filepath = vim.fn.expand("%:p:h")
+					local gitdir = vim.fn.finddir(".git", filepath .. ";")
+					return gitdir and #gitdir > 0 and #gitdir < #filepath
+				end,
+			}
+
+			local config = {
+				options = {
+					icons_enabled = true,
+					component_separators = "",
+					section_separators = "",
+					theme = {
+						normal = { c = { fg = colors.fg, bg = colors.bg } },
+						inactive = { c = { fg = colors.fg, bg = colors.bg } },
+					},
+				},
+				sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_y = {},
+					lualine_z = {},
+					lualine_c = {},
+					lualine_x = {},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_y = {},
+					lualine_z = {},
+					lualine_c = {},
+					lualine_x = {},
+				},
+			}
+
+			local function ins_left(component)
+				table.insert(config.sections.lualine_c, component)
+			end
+
+			local function ins_right(component)
+				table.insert(config.sections.lualine_x, component)
+			end
+
+			ins_left({
+				function()
+					return "▊"
+					-- return ""
+				end,
+				color = { fg = colors.blue },
+				padding = { left = 0, right = 1 },
+			})
+
+			ins_left({
+				function()
+					local filetype_icon, _ =
+						require("nvim-web-devicons").get_icon(vim.fn.expand("%:t"), vim.fn.expand("%:e"))
+					local filename = vim.fn.expand("%:t")
+					return filetype_icon .. " " .. filename
+				end,
+				color = { fg = nil },
+				padding = { left = 0, right = 1 },
+			})
+
+			-- ins_left({
+			--   "filetype",
+			--   icons_enabled = true,
+			--   icon_only = true,
+			--   color = { fg = colors.white },
+			-- })
+			--
+			-- ins_left({ "filename", color = { fg = colors.white } })
+
+			ins_left({
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+				symbols = { error = " ", warn = " ", info = " " },
+				diagnostics_color = {
+					error = { fg = colors.red },
+					warn = { fg = colors.yellow },
+					info = { fg = colors.cyan },
+				},
+			})
+
+			-- ins_left({
+			--   function()
+			--     return "%="
+			--   end,
+			-- })
+
+			-- ins_right({
+			--   "o:encoding",
+			--   fmt = string.upper,
+			--   cond = conditions.hide_in_width,
+			--   color = { fg = colors.green, gui = "bold" },
+			-- })
+
+			-- ins_right({ "branch", icon = "", color = { fg = colors.violet, gui = "bold" } })
+
+			-- ins_right({
+			--   "diff",
+			--   symbols = { added = " ", modified = "󰝤 ", removed = " " },
+			--   diff_color = {
+			--     added = { fg = colors.green },
+			--     modified = { fg = colors.orange },
+			--     removed = { fg = colors.red },
+			--   },
+			--   cond = conditions.hide_in_width,
+			-- })
+
+			ins_right({ "location" })
+			ins_right({ "progress", color = { fg = colors.fg } })
+
+			ins_right({
+				function()
+					return "▊"
+					-- return ""
+				end,
+				color = { fg = colors.blue },
+				padding = { left = 0 },
+			})
+
+			return config
+		end,
+	},
 }
