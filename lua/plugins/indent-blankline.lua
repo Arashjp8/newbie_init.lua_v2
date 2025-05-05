a3839c0b0b714df1c59a16314f29aaf20d4ac2df
arashjp8 <arashjafarpour723@gmail.com>
2025-05-05 14:14:48 +0330

    checkpoint: sync

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,18 +2,18 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     config = function()
-        -- local highlight = {
-        --     "line",
-        -- }
-        -- local hooks = require("ibl.hooks")
-        -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        --     -- vim.api.nvim_set_hl(0, "line", { fg = "#30363D" })
-        --     vim.api.nvim_set_hl(0, "line", { fg = "#35363a" })
-        -- end)
+        local highlight = {
+            "line",
+        }
+        local hooks = require("ibl.hooks")
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            -- vim.api.nvim_set_hl(0, "line", { fg = "#30363D" })
+            vim.api.nvim_set_hl(0, "line", { fg = "#35363a" })
+        end)
         require("ibl").setup({
             indent = {
                 char = "▏",
-                -- highlight = highlight,
+                highlight = highlight,
             },
             scope = { enabled = false },
         })
