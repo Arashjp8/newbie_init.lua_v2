518387ecde9814db43dceb97f72bf3f071e227bb
arashjp8 <arashjafarpour723@gmail.com>
2025-07-22 10:29:23 +0330

    chore: sync

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,44 +1,64 @@
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local harpoon = require("harpoon")
-		harpoon:setup()
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local harpoon = require("harpoon")
+        harpoon:setup()
 
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-		end)
-		vim.keymap.set("n", "<leader>hh", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
+        vim.keymap.set("n", "<leader>a", function()
+            harpoon:list():add()
+        end)
+        vim.keymap.set("n", "<leader>hh", function()
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+        end)
 
-		-- it was originally ctrl + h
-		vim.keymap.set("n", "<leader>1", function()
-			harpoon:list():select(1)
-		end)
-		-- it was originally ctrl + t
-		vim.keymap.set("n", "<leader>2", function()
-			harpoon:list():select(2)
-		end)
-		-- it was originally ctrl + n
-		vim.keymap.set("n", "<leader>3", function()
-			harpoon:list():select(3)
-		end)
-		-- it was originally ctrl + s
-		vim.keymap.set("n", "<leader>4", function()
-			harpoon:list():select(4)
-		end)
+        -- it was originally ctrl + h
+        vim.keymap.set("n", "<leader>1", function()
+            harpoon:list():select(1)
+        end)
+        -- it was originally ctrl + t
+        vim.keymap.set("n", "<leader>2", function()
+            harpoon:list():select(2)
+        end)
+        -- it was originally ctrl + n
+        vim.keymap.set("n", "<leader>3", function()
+            harpoon:list():select(3)
+        end)
+        -- it was originally ctrl + s
+        vim.keymap.set("n", "<leader>4", function()
+            harpoon:list():select(4)
+        end)
 
-		vim.keymap.set("n", "<leader>5", function()
-			harpoon:list():select(5)
-		end)
+        vim.keymap.set("n", "<leader>5", function()
+            harpoon:list():select(5)
+        end)
 
-		vim.keymap.set("n", "<C-S-P>", function()
-			harpoon:list():prev()
-		end)
-		vim.keymap.set("n", "<C-S-N>", function()
-			harpoon:list():next()
-		end)
-	end,
+        vim.keymap.set("n", "<leader>6", function()
+            harpoon:list():select(6)
+        end)
+
+        vim.keymap.set("n", "<leader>7", function()
+            harpoon:list():select(7)
+        end)
+
+        vim.keymap.set("n", "<leader>8", function()
+            harpoon:list():select(8)
+        end)
+
+        vim.keymap.set("n", "<leader>9", function()
+            harpoon:list():select(9)
+        end)
+
+        vim.keymap.set("n", "<leader>0", function() -- Common to map 0 to 10 in leader sequences
+            harpoon:list():select(10)
+        end)
+
+        vim.keymap.set("n", "<C-S-P>", function()
+            harpoon:list():prev()
+        end)
+        vim.keymap.set("n", "<C-S-N>", function()
+            harpoon:list():next()
+        end)
+    end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,69 +2,100 @@
 -- Plugin Configuration: none-ls.nvim
 ---------------------------------------------------------------------
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		---------------------------------------------------------------------
-		-- LSP Formatting Augroup
-		---------------------------------------------------------------------
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        ---------------------------------------------------------------------
+        -- LSP Formatting Augroup
+        ---------------------------------------------------------------------
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        local null_ls = require("null-ls")
+        local null_ls_h = require("null-ls.helpers")
 
-		---------------------------------------------------------------------
-		-- null-ls Setup
-		---------------------------------------------------------------------
-		null_ls.setup({
-			sources = {
-				-- c, c++
-				null_ls.builtins.formatting.clang_format.with({
-					extra_args = {
-						"-style={BasedOnStyle: llvm, IndentWidth: 4, TabWidth: 4}",
-					},
-				}),
-				-- python
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.diagnostics.mypy,
-				-- null_ls.builtins.diagnostics.ruff,
-				-- go
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.golines.with({
-					extra_args = { "--max-len=80", "--base-formatter=gofumpt" },
-				}),
-				-- lua
-				null_ls.builtins.formatting.stylua,
-				-- typescript
-				null_ls.builtins.formatting.prettier.with({
-					-- prefer_local = "node_modules/.bin", -- use project-local Prettier
-					extra_args = { "--tab-width", "4", "--print-width", "120", "--semi", true },
-				}),
-				-- WARNING: deprecated
-				-- null_ls.builtins.formatting.eslint_d,
+        ---------------------------------------------------------------------
+        -- null-ls Setup
+        ---------------------------------------------------------------------
+        null_ls.setup({
+            debug = true,
+            log = { level = "debug", use_console = false },
 
-				-- NOTE: deactivated because of delijan local eslint
-				-- require("none-ls.diagnostics.eslint"),
-			},
-			-- Format on save
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
-		})
+            sources = {
+                -- c, c++
+                null_ls.builtins.formatting.clang_format.with({
+                    extra_args = {
+                        "-style={BasedOnStyle: llvm, IndentWidth: 4, TabWidth: 4}",
+                    },
+                }),
+                -- python
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.diagnostics.mypy,
+                -- null_ls.builtins.diagnostics.ruff,
+                -- go
+                null_ls.builtins.formatting.gofumpt,
+                null_ls.builtins.formatting.goimports_reviser,
+                null_ls.builtins.formatting.golines.with({
+                    extra_args = { "--max-len=80", "--base-formatter=gofumpt" },
+                }),
+                -- lua
+                null_ls.builtins.formatting.stylua,
+                -- typescript
+                null_ls.builtins.formatting.prettier.with({
+                    -- command = "npx",
+                    -- args = null_ls_h.range_formatting_args_factory({
+                    --     "prettier",
+                    --     "--stdin-filepath",
+                    --     "$FILENAME",
+                    -- }, "--range-start", "--range-end", { row_offset = -3, col_offset = -1 }),
+                    extra_args = {
+                        "--tab-width",
+                        "4",
+                        "--print-width",
+                        "120",
+                        "--semi",
+                        true,
+                        -- "--config-precedence",
+                        -- "cli",
+                    },
+                    -- prefer_local = "node_modules/.bin", -- use project-local Prettier
+                    -- filetypes = {
+                    --     "javascript",
+                    --     "typescript",
+                    --     "css",
+                    --     "scss",
+                    --     "html",
+                    --     "json",
+                    --     "yaml",
+                    --     "markdown",
+                    --     "tsx",
+                    --     "jsx",
+                    -- },
+                }),
+                -- WARNING: deprecated
+                -- null_ls.builtins.formatting.eslint_d,
 
-		---------------------------------------------------------------------
-		-- Keybindings
-		---------------------------------------------------------------------
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+                -- NOTE: deactivated because of delijan local eslint
+                -- require("none-ls.diagnostics.eslint"),
+            },
+            -- Format on save
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
+        })
+
+        ---------------------------------------------------------------------
+        -- Keybindings
+        ---------------------------------------------------------------------
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
 }
