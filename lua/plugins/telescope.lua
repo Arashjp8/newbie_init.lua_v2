4ef59509ca634bd5e17dc2be19c4d80e7425a66f
Arash Jafarpour <arashjafarpour723@gmail.com>
2025-08-01 14:59:48 +0330

    chore: sync

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,105 +2,107 @@
 -- Plugin Configuration: Telescope
 ---------------------------------------------------------------------
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.6",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-        },
-        config = function()
-            ---------------------------------------------------------------------
-            -- Telescope Builtin Keybindings
-            ---------------------------------------------------------------------
-            local builtin = require("telescope.builtin")
-            -- local themes = require("telescope.themes")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
+		config = function()
+			---------------------------------------------------------------------
+			-- Telescope Builtin Keybindings
+			---------------------------------------------------------------------
+			local builtin = require("telescope.builtin")
+			-- local themes = require("telescope.themes")
 
-            -- Find files (ignoring node_modules)
-            vim.keymap.set("n", "<Space><Space>", function()
-                builtin.find_files({
-                    hidden = true,
-                    -- Exclude node_modules folder from search
-                    file_ignore_patterns = { "node_modules/.*" },
-                })
-            end, {})
+			-- Find files (ignoring node_modules)
+			vim.keymap.set("n", "<Space><Space>", function()
+				builtin.find_files({
+					hidden = true,
+					-- Exclude node_modules folder from search
+					file_ignore_patterns = { "node_modules/.*" },
+				})
+			end, {})
 
-            -- Telescope itself
-            vim.keymap.set("n", "<leader>t", ":Telescope <CR>", {})
+			-- Telescope itself
+			vim.keymap.set("n", "<leader>t", ":Telescope <CR>", {})
 
-            -- buffer navigation
-            vim.keymap.set("n", "<leader>b", function()
-                builtin.buffers({
-                    sort_lastused = true,
-                    ignore_current_buffer = false,
-                    previewer = false,
-                })
-            end, { desc = "List Open Buffers" })
-            vim.keymap.set("n", "]b", ":bnext <CR>")
-            vim.keymap.set("n", "[b", ":bprev <CR>")
-            --
+			-- buffer navigation
+			vim.keymap.set("n", "<leader>b", function()
+				builtin.buffers({
+					sort_lastused = true,
+					ignore_current_buffer = false,
+					previewer = false,
+				})
+			end, { desc = "List Open Buffers" })
+			vim.keymap.set("n", "]b", ":bnext <CR>")
+			vim.keymap.set("n", "[b", ":bprev <CR>")
+			--
 
-            -- Live Grep
-            vim.keymap.set("n", "<leader>fg", function()
-                builtin.live_grep()
-            end, {})
+			-- -- NOTE: It's been replaced by custom multi_grep
+			-- -- Live Grep
+			-- vim.keymap.set("n", "<leader>fg", function()
+			--     builtin.live_grep()
+			-- end, {})
+			require("config.telescope.multigrep").setup()
 
-            -- LSP References
-            vim.keymap.set("n", "<leader>fr", function()
-                builtin.lsp_references()
-            end)
+			-- LSP References
+			vim.keymap.set("n", "<leader>fr", function()
+				builtin.lsp_references()
+			end)
 
-            -- Help Tags
-            vim.keymap.set("n", "<leader>fh", function()
-                builtin.help_tags()
-            end)
+			-- Help Tags
+			vim.keymap.set("n", "<leader>fh", function()
+				builtin.help_tags()
+			end)
 
-            -- Current Buffer Fuzzy Find
-            vim.keymap.set("n", "<leader>f/", function()
-                builtin.current_buffer_fuzzy_find()
-            end)
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            ---------------------------------------------------------------------
-            -- Telescope UI Select Setup
-            ---------------------------------------------------------------------
-            require("telescope").setup({
-                defaults = {
-                    layout_strategy = "flex",
-                    layout_config = {
-                        width = 0.95,
-                        height = 0.99,
-                        flip_columns = 120,
-                        horizontal = {
-                            prompt_position = "bottom",
-                        },
-                        vertical = {
-                            mirror = false,
-                        },
-                    },
-                    mappings = {
-                        i = {
-                            ["<C-j>"] = require("telescope.actions").move_selection_next,
-                            ["<C-k>"] = require("telescope.actions").move_selection_previous,
-                        },
-                        n = {
-                            ["<C-j>"] = require("telescope.actions").move_selection_next,
-                            ["<C-k>"] = require("telescope.actions").move_selection_previous,
-                        },
-                    },
-                },
-                extensions = {
-                    fzf = {},
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                        require("telescope").load_extension("ui-select"),
-                    },
-                    require("telescope").load_extension("fzf"),
-                },
-            })
-        end,
-    },
+			-- Current Buffer Fuzzy Find
+			vim.keymap.set("n", "<leader>f/", function()
+				builtin.current_buffer_fuzzy_find()
+			end)
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			---------------------------------------------------------------------
+			-- Telescope UI Select Setup
+			---------------------------------------------------------------------
+			require("telescope").setup({
+				defaults = {
+					layout_strategy = "flex",
+					layout_config = {
+						width = 0.95,
+						height = 0.99,
+						flip_columns = 120,
+						horizontal = {
+							prompt_position = "bottom",
+						},
+						vertical = {
+							mirror = false,
+						},
+					},
+					mappings = {
+						i = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
+						n = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
+					},
+				},
+				extensions = {
+					fzf = {},
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+						require("telescope").load_extension("ui-select"),
+					},
+					require("telescope").load_extension("fzf"),
+				},
+			})
+		end,
+	},
 }
